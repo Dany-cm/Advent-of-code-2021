b1e5abb3fc52cb0ff1883b22bdbe61feb4269556
Dany CM <danycmwow@gmail.com>
2021-12-02 22:55:39 +0100

    feat: Day 1 - Sonar Sweep Part 2

--- advent_day1.py
+++ advent_day1.py
@@ -1,8 +1,23 @@
 with open('advent_day1') as file:
     total = file.readlines()
 
+
+def part_one():
     count = 0
     for previous, current in zip(total[:-1], total[1:]):
         if int(previous) < int(current):
             count += 1
     print(count)
+
+
+def part_two():
+    count = 0
+    for previous, current in zip(total, total[3:]):
+        if int(previous) < int(current):
+            count += 1
+    print(count)
+
+
+if __name__ == '__main__':
+    part_one()
+    part_two()
