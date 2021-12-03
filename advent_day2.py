9c7d0078b90c2fc21a943aa5748b96c0ab78284b
Dany CM <danycmwow@gmail.com>
2021-12-03 13:47:48 +0100

    feat: Day 2 - Dive! Part 1

---
+++ advent_day2.py
@@ -0,0 +1,22 @@
+with open('advent_day2') as file:
+    total = file.readlines()
+
+
+def part_one():
+    forward = 0
+    down = 0
+    for word in total:
+        data = word.split()
+        match data[0]:
+            case 'forward':
+                forward += int(data[1])
+            case 'down':
+                down += int(data[1])
+            case 'up':
+                down -= int(data[1])
+    result = forward * down
+    print(f'Horizontal: {forward} Depth: {down} Results: {result}')
+
+
+if __name__ == '__main__':
+    part_one()
